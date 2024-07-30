5757fa58bc0edb9d3762806a680cfc7ca157d574
Elvis Saavedra <elvis.saavedra@nearsure.com>
2024-07-29 22:51:42 -0500

    add deposit modal

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const REFRESH_BUTTON = document.querySelector("#js-refresh")
 const LOADER = document.querySelector("#loader");
 const ACCOUNT_LIST_TABLE_BODY = document.querySelector("#accounts-table tbody");
+const CONFIRM_DEPOSIT_BUTTON = document.querySelector("#confirm-deposit");
+const DEPOSIT_AMOUNT_INPUT = document.querySelector("#deposit-amout");
+let accountId = null;
 
 async function fetchAccounts() {
   LOADER.style.display = "block";
@@ -16,7 +19,7 @@ async function fetchAccounts() {
               <td>${account.accountHolderName}</td>
               <td>${account.balance}</td>
               <td>
-                <button data-account-id=${account.id} class="js-action-deposit btn btn-primary">Deposit</button>
+                <button data-account-id=${account.id} class="js-action-deposit btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Deposit</button>
                 <button data-account-id=${account.id} class="js-action-withdraw btn btn-secondary">Withdraw</button>
               </td>
           </tr>
@@ -26,6 +29,7 @@ async function fetchAccounts() {
     ACCOUNT_LIST_TABLE_BODY.innerHTML = rowHTML;
 
     setDepositActions();
+    setWithdrawActions();
   } catch (e) {
     console.log(e)
   } finally {
@@ -37,18 +41,27 @@ function setDepositActions () {
   const depositButtons = document.querySelectorAll(".js-action-deposit");
   depositButtons.forEach((button) => {
     button.addEventListener("click", async (event) => {
+      accountId = event.target.dataset.accountId;
+    });
+  });
+}
+
+function setWithdrawActions () {
+  const withdrawButtons = document.querySelectorAll(".js-action-withdraw");
+  withdrawButtons.forEach((button) => {
+    button.addEventListener("click", async (event) => {
       const accountId = event.target.dataset.accountId;
-      const amount = prompt("Enter deposit amount");
+      const amount = prompt("Enter withdraw amount");
       if (amount) {
         try {
-          const response = await depositAmount(accountId, amount);
+          const response = await withdrawAmount(accountId, amount);
           if (response) {
-            alert("Deposit successful");
+            alert("Withdraw successful");
             fetchAccounts();
           }
         } catch (error) {
           console.log(error);
-          alert("Deposit failed");
+          alert("Withdraw failed");
         }
       }
     });
@@ -59,6 +72,25 @@ REFRESH_BUTTON.addEventListener("click", () => {
   fetchAccounts();
 })
 
-window.onload = function exampleFunction() {
+CONFIRM_DEPOSIT_BUTTON.addEventListener("click", async () => {
+  try {
+    await depositAmount(accountId, DEPOSIT_AMOUNT_INPUT.value);
+    Swal.fire({
+      title: "Depsit success!",
+      text: "Your balance has been updated!",
+      icon: "success"
+    });
+    DEPOSIT_AMOUNT_INPUT.value = "";
+    fetchAccounts();
+  } catch (error) {
+    Swal.fire({
+      icon: "error",
+      title: "An error happened",
+      text: "Please try again later!",
+    });
+  }
+});
+
+window.onload = function () {
   fetchAccounts();
 };
--- js/services.js
+++ js/services.js
@@ -21,21 +21,35 @@ async function depositAmount(accountId, amount) {
   const body = {
     amount: amount,
   };
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    });
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-    const json = await response.json();
-    return json;
-  } catch (error) {
-    console.error(error.message);
-    throw error
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
   }
+  const json = await response.json();
+  return json;
+}
+
+async function withdrawAmount(accountId, amount) {
+  const url = `${BASE_URL}/accounts/${accountId}/withdraw`;
+  const body = {
+    amount: amount,
+  };
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+  const json = await response.json();
+  return json;
 }
